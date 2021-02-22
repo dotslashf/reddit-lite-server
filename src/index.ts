c831a5b54fc29d686cd4efa62229ca216058cb98
fadhlu <fadhlurahman.i@gmail.com>
2021-02-22 22:04:39 +0700

    add apolloServer

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
-import { Post } from './entities/Post';
+import { HelloResolver } from './resolvers/hello';
 import { __prod__ } from './constant';
 import { MikroORM } from '@mikro-orm/core';
 import mikroOrmConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
-
   orm.getMigrator().up();
-  // const post = orm.em.create(Post, { title: 'my first post' });
-  // await orm.em.persistAndFlush(post);
 
-  const post = await orm.em.find(Post, {});
-  console.log(post);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(3000, () => {
+    console.log('server is running 🚀 on localhost:3000');
+  });
 };
 
 main().catch(err => {
