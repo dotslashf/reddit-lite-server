f030d1b51660076d9f1d403aa1f3a2fcdc3c13b3
fadhlu <fadhlurahman.i@gmail.com>
2021-02-22 21:42:18 +0700

    add migrations

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ import mikroOrmConfig from './mikro-orm.config';
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
 
-  orm.em.create(Post, { title: 'my first post' });
-  orm.em.persistAndFlush(Post);
-  console.log('---sql 2---');
-  orm.em.nativeInsert(Post, { title: 'another first post' });
+  orm.getMigrator().up();
+  // const post = orm.em.create(Post, { title: 'my first post' });
+  // await orm.em.persistAndFlush(post);
+
+  const post = await orm.em.find(Post, {});
+  console.log(post);
 };
 
 main().catch(err => {
