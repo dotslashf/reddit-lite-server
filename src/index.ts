e0e956098744504e4341ae5fd6f1a021b05992c2
fadhlu <fadhlurahman.i@gmail.com>
2021-02-24 12:20:04 +0700

    login and register

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import mikroOrmConfig from './mikro-orm.config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
+import { UserResolver } from './resolvers/user';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
@@ -16,7 +17,7 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
     context: () => ({ em: orm.em }),
