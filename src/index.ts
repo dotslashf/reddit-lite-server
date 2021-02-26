5e89e99aa143dadabc595271f1c5a564f081d1fb
fadhlu <fadhlurahman.i@gmail.com>
2021-02-26 13:17:37 +0700

    .

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import { PostResolver } from './resolvers/post';
 import { HelloResolver } from './resolvers/hello';
-import { __prod__ } from './constant';
+import { __prod__, PORT } from './constant';
 import { MikroORM } from '@mikro-orm/core';
 import mikroOrmConfig from './mikro-orm.config';
 import express from 'express';
@@ -48,8 +48,8 @@ const main = async () => {
 
   apolloServer.applyMiddleware({ app });
 
-  app.listen(4000, () => {
-    console.log('server is running 🚀 on localhost:3000');
+  app.listen(PORT, () => {
+    console.log(`server is running 🚀 on localhost:${PORT}`);
   });
 };
 
